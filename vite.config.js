fae9b92a2ffd60994040f2bafdc4976afb2982ee
yichen4682-dot <yichen4682@gmail.com>
2026-01-19 23:02:09 +0800

    revise

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: process.env.Node_ENV === 'production' ? '/vite-project/' : '/',
+  base: '/vite-project/',
   plugins: [react()],
+  build: {
+    outDir: 'docs'
+  }
 })
