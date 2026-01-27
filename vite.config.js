9fa23434c145062f60070125fdad82184f587942
yichen4682-dot <yichen4682@gmail.com>
2026-01-28 04:34:29 +0800

    revised issue

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: '/vite-project/',
+  base: "/vite-project/",
   plugins: [react()],
-  build: {
-    outDir: 'docs'
-  }
-})
+});
